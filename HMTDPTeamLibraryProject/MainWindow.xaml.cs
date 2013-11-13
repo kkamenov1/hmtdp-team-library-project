9a6cf9fe216a35af672b7d14ffdcb68f6a9deee9
stanev.plamen@gmail.com <stanev.plamen@gmail.com@2d41c407-55ca-3958-08b6-b628f854cf0f>
2013-11-13 08:39:34 +0000

    adding the first part of the project`s MVVM pattern structure

--- HMTDPTeamLibraryProject/MainWindow.xaml.cs
+++ HMTDPTeamLibraryProject/MainWindow.xaml.cs
@@ -23,6 +23,58 @@ namespace HMTDPTeamLibraryProject
         public MainWindow()
         {
             InitializeComponent();
+            mainFrame.Navigate(new LibraryListPage());
         }
+
+        void OnHomeButtonClick(object sender, RoutedEventArgs args)
+        {
+            //mainFrame.Navigate(new Welcome());
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            //base.OnClosing(e);
+            e.Cancel = true;
+            //Environment.Exit(0);
+           // MessageBoxResult askResult = MessageBox.ShowQuestion("Do you want to Exit?", "The scheduler cannot remind you during offline mode!\nYes: Exit (offline mode)\nNo: Minimize to tray (working reminder)\nCancel: Cancel command", true);
+            if (true) //(askResult == MessageBoxResult.Yes)
+            {
+                Environment.Exit(0);
+            }
+            //if (askResult == MessageBoxResult.No)
+            //{
+            //    WindowState = WindowState.Minimized;
+            //}
+            ////this.Hide();
+            ////
+        }
+
+        private void OnExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            //MessageBoxResult askResult = MessageBox.ShowQuestion("Do you want to Exit?", "The scheduler cannot remind you during offline mode!\nYes: Exit (offline mode)\nNo: Minimize to tray (working reminder)\nCancel: Cancel command", true);
+            //if (askResult == MessageBoxResult.Yes)
+            //{
+            //    Environment.Exit(0);
+            //}
+            //if (askResult == MessageBoxResult.No)
+            //{
+            //    WindowState = WindowState.Minimized;
+            //}
+        }
+
+        private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void OnInfoButtonClick(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void OnCheckButtonClick(object sender, RoutedEventArgs e)
+        {
+          
+        }    
     }
 }
