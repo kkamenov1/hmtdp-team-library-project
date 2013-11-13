254cc66e293564a845d99891f96e14bcc77892f9
stanev.plamen@gmail.com <stanev.plamen@gmail.com@2d41c407-55ca-3958-08b6-b628f854cf0f>
2013-11-13 08:42:46 +0000

    adding new folders

---
+++ HMTDPTeamLibraryProject/ViewModels/DataPersister.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace HMTDPTeamLibraryProject.ViewModels
+{
+    public class DataPersister
+    {
+        public static IEnumerable<ArticleViewModel> GetArticle(string articleStoreDocumentPath)
+        {
+            var articleDocumentRoot = XDocument.Load(articleStoreDocumentPath).Root;
+
+            var articleVMs =
+                           from articleElement in articleDocumentRoot.Elements("article")
+                           select new ArticleViewModel()
+                           {
+                               Author = articleElement.Element("author").Value,
+                               Title = articleElement.Element("title").Value,
+                               Year = int.Parse(articleElement.Element("year").Value),
+                               ImagePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), articleElement.Element("image").Value),
+                           };
+            return articleVMs;
+        }
+
+        internal static void AddArticle(string documenPath, ArticleViewModel article)
+        {
+            var root = XDocument.Load(documenPath).Root;
+            root.Add(new XElement("article",
+                new XElement("author", article.Author),
+                new XElement("title", article.Title),
+                new XElement("year", article.Year),
+                new XElement("image", article.ImagePath)));
+            root.Document.Save(documenPath);
+        }
+    }
+}
