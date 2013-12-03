6df97d3b641e765cc7ba67df20b70bb8caf7aca2
hristobak@gmail.com <hristobak@gmail.com@2d41c407-55ca-3958-08b6-b628f854cf0f>
2013-12-03 16:32:58 +0000

    Added interface for Sort -> ISortable.cs

--- trunk/HMTDPTeamLibraryProject/Search and Sort/Sort.cs
+++ trunk/HMTDPTeamLibraryProject/Search and Sort/Sort.cs
@@ -1,4 +1,5 @@
-﻿using HMTDPTeamLibraryProject.ViewModels;
+﻿using HMTDPTeamLibraryProject.Search_and_Sort;
+using HMTDPTeamLibraryProject.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,39 +10,54 @@ using System.Threading.Tasks;
 namespace HMTDPTeamLibraryProject
 {
     //TODO: We can add an interface here.
-    public class Sort
+    public class Sort : ISortable
     {
         private IEnumerable<ArticleViewModel> articles = DataPersister.GetArticle(MainWindow.mainFilePath);
+
+        public IEnumerable<ArticleViewModel> Search(string searchedWord, string property)
+        {
+            IList<ArticleViewModel> result = new IList<ArticleViewModel>();
+            foreach (var item in articles)
+            {
+
+                if (item.Author == property && item.Author.Contains(searchedWord))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return articles;
+        }
 
         public IEnumerable<ArticleViewModel> SortByAttribute(string attributName)
         {
             //foreach (var prop in typeof(ArticleViewModel).GetProperties())
             //{
-                PropertyInfo givenPropertyName = typeof(ArticleViewModel).GetProperty(attributName);
-                if (givenPropertyName == typeof(ArticleViewModel).GetProperty("Author"))
-                {
-                    articles = this.articles.OrderBy(x => x.Author);
-                }
-                else if (givenPropertyName == typeof(ArticleViewModel).GetProperty("Category"))
-                {
-                    articles = this.articles.OrderBy(x => x.Category);
-                }
-                else if (givenPropertyName == typeof(ArticleViewModel).GetProperty("Contents"))
-                {
-                    articles = this.articles.OrderBy(x => x.Contents);
-                }
-                else if (givenPropertyName == typeof(ArticleViewModel).GetProperty("Title"))
-                {
-                    articles = this.articles.OrderBy(x => x.Title);
-                }
-                else if (givenPropertyName == typeof(ArticleViewModel).GetProperty("Year"))
-                {
-                    articles = this.articles.OrderBy(x => x.Year).ThenBy(x => x.Month).ThenBy(x => x.Day);
-                }
-                else
-                {
-                    //TODO: Maybe we should throw an exception here?
-                }
+            PropertyInfo givenPropertyName = typeof(ArticleViewModel).GetProperty(attributName);
+            if (givenPropertyName == typeof(ArticleViewModel).GetProperty("Author"))
+            {
+                articles = this.articles.OrderBy(x => x.Author);
+            }
+            else if (givenPropertyName == typeof(ArticleViewModel).GetProperty("Category"))
+            {
+                articles = this.articles.OrderBy(x => x.Category);
+            }
+            else if (givenPropertyName == typeof(ArticleViewModel).GetProperty("Contents"))
+            {
+                articles = this.articles.OrderBy(x => x.Contents);
+            }
+            else if (givenPropertyName == typeof(ArticleViewModel).GetProperty("Title"))
+            {
+                articles = this.articles.OrderBy(x => x.Title);
+            }
+            else if (givenPropertyName == typeof(ArticleViewModel).GetProperty("Year"))
+            {
+                articles = this.articles.OrderBy(x => x.Year).ThenBy(x => x.Month).ThenBy(x => x.Day);
+            }
+            else
+            {
+                //TODO: Maybe we should throw an exception here?
+            }
             //}
 
             JustArrange(articles);
